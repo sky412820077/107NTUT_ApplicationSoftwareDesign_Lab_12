13eac245a2c759984867d892277c9695402cdebb
sky412820077 <sky412820077@gmail.com>
2018-12-20 14:48:15 +0800

    lab12

--- MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+
 import com.google.gson.Gson;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -20,39 +21,19 @@ import com.squareup.okhttp.Response;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    findViewById(R.id. btn_query ).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick (View v){
-            Request req = new Request.Builder()
-                    .url("https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e -dc5c -4268 -a725 -d04cc262172b").build();
-            new OkHttpClient().newCall(req).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    Log.e("查詢失敗 ", e.toString());
-                }
 
-                @Override
-                public void onResponse(Call call, Response response)
-                        throws IOException {
-                    sendBroadcast(new Intent("MyMessage")
-                            .putExtra("json", response.body().string()));
-                }
-            });
-        }
-    });
     private BroadcastReceiver recviver = new BroadcastReceiver() {
         @Override
         public void onReceive (Context context , Intent intent) {
 
-            Data data = new Gson().fromJson
-                (intent.getExtras().getString( "json" ), Data. class );
+            Data data = new Gson().fromJson(intent.getExtras().getString( "json" ), Data. class );
             final String[] items = new String[data. result .results .length ];
 
-            for (int i= 0; i<items. length ; i++)
+            for (int i= 0; i<items. length ; i++){
                 items[i] = "\n列車即將進入 :" +
                         data. result .results [i]. Station +
                          "\n列車行駛目的地 :" +
-                        data. result .results [i]. Destination;
+                        data. result .results [i]. Destination;}
 
             MainActivity. this .runOnUiThread( new Runnable() {
                 @Override
@@ -75,15 +56,40 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.btn_query).setOnClickListener(new
         View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            ...
-        }
+            @Override
+            public void onClick(View v) {
+                Request req = new Request.Builder()
+                        .url("https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b").build();
+                new OkHttpClient().newCall(req).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
+                        Log.e("查詢失敗 ", e.toString());
+                    }
+
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                        sendBroadcast(new Intent("MyMessage").putExtra("json", response.body().string()));
+                    }
+
+
+
+                });
+            }
         });
     }
     @Override
     protected void onDestroy(){
         super.onDestroy();
         unregisterReceiver(recviver);
+    }
+}
+class Data {
+    Result result ;
+    class Result{
+        Results[] results ;
+        class Results{
+            String Station ;
+            String Destination ;
+        }
     }
 }
